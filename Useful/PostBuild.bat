11ffc8255bb7d7348be285b4880e301fe39d0481
aphawkins <aph828@gmail.com>
2021-01-28 22:24:33 +0000

    delete obsolete files

--- Useful/PostBuild.bat
+++
@@ -1,5 +0,0 @@
-echo *** Useful.Windows.dll PostBuild ***
-echo *** Start copying useful config file ***
-xcopy /d /y ..\..\..\Useful\Config\Useful.Config.xml
-echo *** End copying useful config file ***
-echo *** End Useful.Windows.dll PostBuild ***
