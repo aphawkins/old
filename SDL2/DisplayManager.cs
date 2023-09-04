e98029e6bb73abe70a83dbdd3f7aa88fdd7ada7b
Andy Hawkins <aph828@gmail.com>
2023-09-04 20:31:28 +0100

    SDL keyboard input

---
+++ SDL2/DisplayManager.cs
@@ -0,0 +1,68 @@
+﻿namespace MonoLife.Client
+{
+    using System;
+    using SDL2;
+
+    public class DisplayManager
+    {
+        protected static IntPtr window;
+        protected static uint windowID;
+        protected static IntPtr context;
+        protected static bool isClosed = false;
+
+        public static void CreateDisplay(string title, int width, int height)
+        {
+            if (SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING) != 0)
+            {
+                Console.WriteLine("SDL_Init Error:", SDL.SDL_GetError());
+            }
+
+            window = SDL.SDL_CreateWindow(title, SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED, width, height, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+            windowID = SDL.SDL_GetWindowID(window);
+
+            _ = SDL.SDL_GL_CreateContext(window);
+        }
+
+        public static void UpdateDisplay()
+        {
+            SDL.SDL_GL_SwapWindow(window);
+            _ = SDL.SDL_UpdateWindowSurface(window);
+        }
+
+        public static bool WaitForClose => isClosed;
+
+        public static void InputDisplay(SDL.SDL_Event evt)
+        {
+            while (SDL.SDL_PollEvent(out evt) != 0)
+            {
+                if (evt.type == SDL.SDL_EventType.SDL_QUIT)
+                {
+                    isClosed = true;
+                    break;
+                }
+
+                if (evt.type == SDL.SDL_EventType.SDL_KEYDOWN)
+                {
+                    if (evt.key.keysym.sym == SDL.SDL_Keycode.SDLK_ESCAPE)
+                    {
+                        isClosed = true;
+                        break;
+                    }
+                }
+
+                if (evt.window.windowEvent == SDL.SDL_WindowEventID.SDL_WINDOWEVENT_RESIZED)
+                {
+                    _ = evt.window.data1;
+                    _ = evt.window.data2;
+                }
+            }
+        }
+
+        public static void DestoryDisplay()
+        {
+            //SDL.SDL_GL_DeleteContext(context);
+            SDL.SDL_DestroyWindow(window);
+            SDL.SDL_Quit();
+        }
+    }
+}
---
+++ SDL2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MonoLife
+{
+    using System;
+    using MonoLife.Client;
+    using SDL2;
+
+    internal class MainClass
+    {
+        protected static SDL.SDL_Event systemEvent;
+
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            DisplayManager.CreateDisplay("Hello Game", 640, 480);
+            // game initalization like GL commands for preparations or game's arguments too like for statement with if else etc..
+
+
+            // end of game initalization
+            while (!DisplayManager.WaitForClose)
+            {
+                DisplayManager.UpdateDisplay();
+                // game logic
+
+
+                // end of game logic
+                DisplayManager.InputDisplay(systemEvent);
+                // game input manager
+
+
+                // end of game input managers
+            }
+            // destory or deletion of game logic like shader.CleanUp or model.CleanUp or GL comand GL.DeleteBuffer(vbo);
+
+
+            // end of destory or deletion of game logic
+            DisplayManager.DestoryDisplay();
+        }
+    }
+}
